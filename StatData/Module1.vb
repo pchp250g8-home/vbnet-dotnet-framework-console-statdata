d925cb00991a25bcfcecd7b6840b491291283c2a
pchp250g8-home <pchp250g8@tutamail.com>
2025-10-01 16:15:21 +0300

    Created project StatData
    StatData. VBNet Tutorials. Platform DotNet FrameWork. Console Application. Alhorithms.

---
+++ StatData/Module1.vb
@@ -0,0 +1,64 @@
+﻿Imports System
+
+Module Module1
+
+    Sub Main()
+        Dim AData(10) As Integer
+        Dim Rnd As New Random()
+        Dim nArrayLen = AData.Length
+        Dim nMediane = 0
+        Dim nModes = New List(Of Integer)()
+        '----------------------------------------------------------------------------------------------------
+        nModes.Clear()
+        Console.WriteLine("Generating Statictic Data:")
+        For i = 0 To nArrayLen - 1
+            AData(i) = Rnd.Next(40)
+            Console.Write("{0} ", AData(i))
+        Next
+        Array.Sort(AData)
+        Console.WriteLine()
+        Console.WriteLine("Sorted Data:")
+        For i = 0 To nArrayLen - 1
+            Console.Write("{0} ", AData(i))
+        Next
+        '----------------------------------------------------------------------------------------------------
+        If (nArrayLen Mod 2 = 0) Then
+            Dim nIndex1 = nArrayLen / 2 - 1
+            Dim nIndex2 = nIndex1 + 1
+            nMediane = Convert.ToInt32((AData(nIndex1) + AData(nIndex2)) / 2)
+        Else
+            Dim nIndex = Convert.ToInt32((nArrayLen + 1) / 2) - 1
+            nMediane = AData(nIndex)
+        End If
+        '----------------------------------------------------------------------------------------------------
+        For i = 0 To nArrayLen - 1
+            Dim nSameElems = 0
+            For j = 0 To nArrayLen - 1
+                If (AData(i) = AData(j)) Then
+                    nSameElems += 1
+                End If
+            Next
+            If (nSameElems > 1) Then
+                nModes.Add(AData(i))
+            End If
+        Next
+        '----------------------------------------------------------------------------------------------------
+        Console.WriteLine()
+        Console.WriteLine("Statistic Charectiristics:")
+        Console.WriteLine("Count:{0}", nArrayLen)
+        Console.WriteLine("Minimum:{0}", AData.Min())
+        Console.WriteLine("Maximum:{0}", AData.Max())
+        Console.WriteLine("Amplitude:{0}", AData.Max() - AData.Min())
+        Console.WriteLine("Average Value:{0}", AData.Average())
+        Console.WriteLine("Mediane:{0}", nMediane)
+        If (nModes.Count > 0) Then
+            Dim nModesCount = nModes.Count
+            Console.Write("Mode:")
+            For i = 0 To nModesCount - 1
+                Console.Write("{0} ", nModes(i))
+            Next
+            Console.WriteLine()
+        End If
+    End Sub
+
+End Module
---
+++ StatData/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Общие сведения об этой сборке предоставляются следующим набором
+' набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+' связанные со сборкой.
+
+' Проверьте значения атрибутов сборки
+
+<Assembly: AssemblyTitle("StatData")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("StatData")>
+<Assembly: AssemblyCopyright("Copyright ©  2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+<Assembly: Guid("a3d3c027-3f82-4919-ab3b-bb3bb68430f6")>
+
+' Сведения о версии сборки состоят из следующих четырех значений:
+'
+'      Основной номер версии
+'      Дополнительный номер версии
+'   Номер сборки
+'      Редакция
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
